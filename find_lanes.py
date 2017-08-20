22d29139abfe5f42d48295cdc6b3850a9053f039
Gitty-McHubface <Gitty-McHubface@users.noreply.github.com>
2017-08-20 16:04:56 -0700

    tmp

--- find_lanes.py
+++ find_lanes.py
@@ -7,9 +7,9 @@ import os
 
 CALIBRATION_DIR = './camera_cal'
 
-DEBUG_IMAGE = True
+DEBUG_IMAGE = False
 USE_SAVED_CORRECTION = True
-TEST_IMAGE = './test_images/test5.jpg'
+TEST_IMAGE = './test_images/test6.jpg'
 
 
 def calibrate_camera(calibration_dir, file_pattern='calibration*.jpg'):
@@ -124,10 +124,10 @@ def warp(image, src=None, dst=None):
                           [188, 720],
                           [594, 448]])
 
-        dst = np.float32([[920, 0],
-                          [920, 720],
-                          [340, 720],
-                          [340, 0]])
+        dst = np.float32([[900, 0],
+                          [900, 720],
+                          [300, 720],
+                          [300, 0]])
 
 
     m = cv2.getPerspectiveTransform(src, dst)
@@ -142,10 +142,10 @@ def warp(image, src=None, dst=None):
         plt.show()
 
         plt.imshow(warped, cmap='gray')
-        plt.plot((920, 920), (0, 720), 'r')
-        plt.plot((920, 340), (720, 720), 'r')
-        plt.plot((340, 340), (0, 720), 'r')
-        plt.plot((340, 920), (0, 0), 'r')
+        plt.plot((900, 900), (0, 720), 'r')
+        plt.plot((900, 300), (720, 720), 'r')
+        plt.plot((300, 300), (0, 720), 'r')
+        plt.plot((300, 900), (0, 0), 'r')
         plt.show()
 
     return warped
@@ -238,6 +238,90 @@ def image_pipeline(image, ksize=3):
     warped = warp(masked)
     return warped
    
+
+def get_poly(overhead_image):
+    histogram = np.sum(overhead_image[overhead_image.shape[0] // 2:,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((overhead_image, overhead_image, overhead_image))*255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(overhead_image.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = overhead_image.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+    
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = overhead_image.shape[0] - (window+1)*window_height
+        win_y_high = overhead_image.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+    
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+    
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, overhead_image.shape[0]-1, overhead_image.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.show()
+
+    return (left_fit, right_fit)
+
     
 def main():
     mtx = np.array([])
@@ -260,6 +344,9 @@ def main():
     overhead = image_pipeline(undist)
     plt.imshow(overhead, cmap='gray')
     plt.show()
+
+    left_fit, right_fit = get_poly(overhead)
+    
 
 
 
